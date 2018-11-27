b4fc6a3fc8fa9fafd50de20d1c27d5c0692f4003
bchain618 <bchain618@gmail.com>
2018-11-27 16:00:13 +0800

    feature#request address immediately after handshaking

--- peer.go
+++ peer.go
@@ -99,6 +99,8 @@ func (peer *Peer) Start() error {
 			return err
 		}
 	}
+	// request address immediately after handshaking
+	go peer.sendAddrReqMessage()
 	go peer.recvHandler()
 	go peer.sendHandler()
 	return nil
@@ -187,6 +189,12 @@ func (peer *Peer) readVersionAckMessage() error {
 		return err
 	}
 	return nil
+}
+
+// read version ack message
+func (peer *Peer) sendAddrReqMessage() error {
+	addreqMsg := &message.AddrReq{}
+	return peer.conn.SendMessage(addreqMsg)
 }
 
 // read specified type message From peer.
